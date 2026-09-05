c00e0f37fd1da2f9c2b7e323a5387b1e791da082
Treasure123-school <treasurehomeschool@gmail.com>
2026-09-05 15:27:35 +0100

    feat: show fade slideshow from 1024px and align carousel width to container

--- client/src/components/home/PromoBanners.tsx
+++ client/src/components/home/PromoBanners.tsx
@@ -8,7 +8,7 @@ interface PromoBannersProps {
   isLoading?: boolean
 }
 
-const XL_QUERY = '(min-width: 2560px)'
+const XL_QUERY = '(min-width: 1024px)'
 
 function useMediaQuery(query: string) {
   const [matches, setMatches] = useState(() => window.matchMedia(query).matches)
